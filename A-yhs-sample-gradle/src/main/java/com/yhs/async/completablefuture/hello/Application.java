f44fb63e42a44af203a8670b54e52e1546d817dc
hyunsu-yang <hyunsu.yang@navercorp.com>
2018-04-06 14:29:27 +0900

    spring async 설명 추가

--- A-yhs-sample-gradle/src/main/java/com/yhs/async/completablefuture/hello/Application.java
+++ A-yhs-sample-gradle/src/main/java/com/yhs/async/completablefuture/hello/Application.java
@@ -8,6 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+/*
+ * 스프링에서 @Async로 비동기처리하기
+ * 자바 설정Java configuration으로 비동기 처리enabling asynchronous processing를 쓰려면 간단히 설정 클래스에 @EnableAsync를 추가해주기만 하면 된다
+	annotation : 
+	기본값, @EnableAsync은 스프링의 Async 어노테이션을 감지하며 EJB 3.1javax.ejb.Asynchronous; 이 옵션으로 사용자 정의된 다른 어노테이션 또한 감지할 수 있다.
+	mode : 
+	사용해야할 advice의 타입을 가르킨다 - JDK proxy 기반 또는 AspectJ weaving.
+	proxyTargetClass 
+	: 사용해야할 proxy의 타입을 가르킨다 - CGLIB 또는 JDK; 이 속성값은 오직 mode가 AdviceMode.PROXY 로 설정되어 있을때만 유효하다.
+	order 
+	: sets the order in which AsyncAnnotationBeanPostProcessor 가 적용해야할 순서를 설정한다; 단지 모든 현존의 프록시를 고려하기 위해 기본값으로 마지막부터 실행된다.
+ */
+
 @SpringBootApplication
 @EnableAsync
 public class Application {
@@ -21,13 +34,13 @@ public class Application {
     public Executor asyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
         executor.setQueueCapacity(500);
         executor.setThreadNamePrefix("GithubLookup-");
         executor.initialize();
         return executor;
     }
-
+    
 
 }
--- A-yhs-sample-gradle/src/main/java/com/yhs/async/completablefuture/hello/GitHubLookupService.java
+++ A-yhs-sample-gradle/src/main/java/com/yhs/async/completablefuture/hello/GitHubLookupService.java
@@ -9,6 +9,15 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+/*
+ *@Async 는 두가지 제약사항
+	1. public 메소드에만 적용해야한다
+	2. 셀프 호출 self invocation – 같은 클래스안에서 async 메소드를 호출 – 은 작동하지않음
+	(이유는 간단한데 메소드가 public이어야 프록시가 될수 있기 때문이고 셀프호출은 프록시를 우회하고 해당 메소드를 직접 호출하기때문에 작동하지않는 것)
+
+
+*/
+
 @Service
 public class GitHubLookupService {
 
@@ -27,7 +36,10 @@ public class GitHubLookupService {
         User results = restTemplate.getForObject(url, User.class);
         // Artificial delay of 1s for demonstration purposes
         Thread.sleep(1000L);
-        return CompletableFuture.completedFuture(results);
+        
+       // Future<String> test =  new AsyncResult<String>("tset");
+        
+       return CompletableFuture.completedFuture(results);
     }
 
 }
